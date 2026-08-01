8949e3500549c9ca43dc510c6bb5704420cdf968
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-08-01 17:41:33 +0600

    HourlyForecast Add

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
           </div>
         </div>
 
-        {/* <HourlyForecast weather={weather} /> */}
+        <HourlyForecast weather={weather} />
         {/* <DailyForecast weather={weather} /> */}
 
         <Footer /> 
